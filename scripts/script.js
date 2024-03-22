d3f9c906ff30c75bac68d56394c16d7e47576029
$ovm <79712068+ovasam@users.noreply.github.com>
2024-03-21 20:49:03 -0500

    Added multiple files
    added scripts of js, functionality and translate buttons

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+// document.getElementById("login").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+    
+//     // Aquí puedes agregar tu lógica de validación y autenticación de inicio de sesión
+    
+//     if (username === "usuario" && password === "contraseña") {
+//       alert("Inicio de sesión exitoso");
+//     } else {
+//       document.getElementById("error-message").textContent = "Credenciales inválidas";
+//     }
+//   });
+  
+//   document.getElementById("register").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     var newUsername = document.getElementById("new-username").value;
+//     var newPassword = document.getElementById("new-password").value;
+    
+//     // Aquí puedes agregar tu lógica de validación y registro de nuevos usuarios
+    
+//     alert("Registro exitoso");
+//   });
+
+
+
+
+// const Translate = document.querySelector(".translateE");
+
+// Translate.addEventListener("click", function() {
+//     const texto = document.querySelector(".texto")
+//     texto.innerHTML = `<p>My name is Samuel. I’ve always been passionate about computing, and from a very young age, I’ve acquired a great deal of knowledge about this fascinating world. I’ve been drawn to the programming side of things, understanding how code works, the intricacies of logistics, and the myriad of possibilities that can be achieved with a command line. Despite moments of frustration, I’ve persevered. Today marks my greatest achievement yet—I’ve overcome many challenges that I once left behind
+//     </p><a class="translateS" href="#translateE"><box-icon name='transfer'></box-icon></a>
+// `;
+    
+// })
+
+
+
+
---
+++ scripts/trsl_db.js
@@ -0,0 +1,34 @@
+console.log("Estoy activo")
+const langEl = document.querySelector('.btns_translate')
+const link = document.querySelectorAll('.link_trans')
+const text = document.querySelector('.texto')
+const tittle = document.querySelector('.me')
+
+
+link.forEach(el => {
+    el.addEventListener('click', () => {
+        // console.log('diste click a un boton de translate')
+        langEl.querySelector('.active').classList.remove('active');
+        el.classList.add('active');
+
+        const attr = el.getAttribute('language')
+
+        text.textContent = data[attr].texto
+        tittle.textContent = data[attr].me
+
+    }); 
+});
+
+
+var data = {
+    "spanish":
+    {
+        "me": "Hola, Soy Samuel",
+        "texto": "mi nombre es Samuel, siempre me ha gustado mucho la informatica y desde muy pequeño he adquirido mucho conocimiento sobre este grandioso mundo. Desde que tengo memoria he sentido atracción por el lado de la programación y el como funciona un codigo, la logística y la cantidad de cosas que se pueden llegar a hacer con una linea de comandos. He dejado varias veces el ambito de programar por frustración, pero aquí esta mí mas grande avance hasta el día de hoy, luego de varios intentos, he logrado concluir muchos desiertos que he dejado en el pasado."
+    },
+    "english":
+    {
+        "me": "Hi, i'm Samuel",
+        "texto": "My name is Samuel. I've always been really into computers, and I've been soaking up knowledge about this amazing world since I was little. As far back as I can remember, I've been drawn to the programming side of things and how code works, the logistics, and all the cool stuff you can do with a command line. I've stepped away from programming a few times out of frustration, but here's my biggest breakthrough yet. After several tries, I've managed to conquer many of the challenges I've faced in the past."
+    }
+}
