2963fef12289a6d84c16266cc379a9ccaf6b29f1
$ovm <79712068+ovasam@users.noreply.github.com>
2024-03-23 22:46:36 -0500

    Added more styles
    styled form box more, added more css

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,39 @@
 console.log("Estoy activo con el contacto")
 const btncontact = document.querySelector('.btn___contact')
-const close_modal = document.querySelector('#close_modal')
+// const close_modal = document.querySelector('#close_modal')
 const contact_modal = document.querySelector('.contact_modal')
 
 
 btncontact.addEventListener('click', (e) => {
     e.preventDefault();
-        console.log('diste click a un boton de contacto')
+        console.log('Conctactando desde la Navbar')
         contactando();
     });
 
-close_modal.addEventListener('click', (e) => {
-    e.preventDefault();
-    close___modal();
-})
 
+function listener(){
+    console.log('escuchando el teclado')
+    document.addEventListener('keydown', function(event) {
+        if(event.key === 'Escape') {
+            close___modal();
+        }
+    })
+}
+function unlistener(){
+    console.log('deje de escuchar el teclado')
+    document.removeEventListener('keydown', function(event) {
+        if(event.key === 'Escape') {
+            close___modal();
+        }
+    })
+}
 
 function contactando() {
     contact_modal.classList.add('contact_modal--show')
+    listener()
 }
 
 function close___modal() {
     contact_modal.classList.remove('contact_modal--show')
+    unlistener();
 }
